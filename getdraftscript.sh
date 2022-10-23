f2913d60a9eff622e0cf129f2b0c81172f9925a7
dblblaster <104750415+dblblaster@users.noreply.github.com>
2022-10-23 09:00:58 +0200

    Update scripts for backup

--- getdraftscript.sh
+++ getdraftscript.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# https://github.com/lutris/lutris/blob/9961fc3c1a921cf1c48f4fcedc66fc25e3f28fd0/lutris/settings.py#L45
+game_slug=$(echo "$1" | grep -Po '(?<=lutris\:)[-.\w]+(?=\?)')
+revision=$(echo "$1" | grep -Po '(?<=\?revision=)\w+$')
 
-game_slug=$(echo "$1" | grep -Po '(?<=lutris\:).*(?=\?)')
-revision=$(echo "$1" | grep -Po '(?<=\?revision=).*$')
-echo $game_slug
-echo $revision
+# https://github.com/lutris/lutris/blob/9961fc3c1a921cf1c48f4fcedc66fc25e3f28fd0/lutris/settings.py#L45
 url="https://lutris.net/api/installers/games/$game_slug/revisions/$revision"
 apiresponse=$(curl "$url")
+
 slug=$(echo "$apiresponse" | jq '.slug' | sed -e 's/\"//g')
 mkdir -p "games/$game_slug"
-echo "$apiresponse" | jq > "games/$game_slug/$slug".json
-printf "### url\n\n$url\n\n" > "games/$game_slug/$slug".md
-printf "### game_slug\n\n$(echo "$apiresponse" | jq '.game_slug' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
-printf "### runner\n\n$(echo "$apiresponse" | jq '.runner' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
-printf "### version\n\n$(echo "$apiresponse" | jq '.version' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
-printf "### description\n\n$(echo "$apiresponse" | jq '.description' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
-printf "### notes\n\n\`\`\`\n$(echo "$apiresponse" | jq '.notes' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
-printf "### content\n\n\`\`\`\n$(echo "$apiresponse" | jq '.content' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
+echo "$apiresponse" | jq > "games/$game_slug/$slug-rev$revision".json
+printf "### url\n\n$url\n\n" > "games/$game_slug/$slug-rev$revision".md
+printf "### game_slug\n\n$game_slug\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### runner\n\n$(echo "$apiresponse" | jq '.runner' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### version\n\n$(echo "$apiresponse" | jq '.version' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### description\n\n$(echo "$apiresponse" | jq '.description' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### notes\n\n\`\`\`\n$(echo "$apiresponse" | jq '.notes' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### credits\n\n\`\`\`\n$(echo "$apiresponse" | jq '.credits' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### reason\n\n\`\`\`\n$(echo "$apiresponse" | jq '.reason' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug-rev$revision".md
+printf "### content\n\n\`\`\`\n$(echo "$apiresponse" | jq '.content' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug-rev$revision".md
---
+++ getfinalscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+slug=$(echo "$1" | grep -Po '(?<=lutris\:)[-.\w]+')
+
+
+# https://github.com/lutris/lutris/blob/9961fc3c1a921cf1c48f4fcedc66fc25e3f28fd0/lutris/settings.py#L43
+url="https://lutris.net/api/installers/$slug"
+apiresponse=$(curl "$url")
+
+game_slug=$(echo "$apiresponse" | jq '.results | .[] | .game_slug' | sed -e 's/\"//g')
+mkdir -p "games/$game_slug"
+echo "$apiresponse" | jq '.results | .[]' > "games/$game_slug/$slug".json
+printf "### url\n\n$url\n\n" > "games/$game_slug/$slug".md
+printf "### game_slug\n\n$game_slug\n\n" >> "games/$game_slug/$slug".md
+printf "### runner\n\n$(echo "$apiresponse" | jq '.results | .[] | .runner' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
+printf "### version\n\n$(echo "$apiresponse" | jq '.results | .[] | .version' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
+printf "### description\n\n$(echo "$apiresponse" | jq '.results | .[] | .description' | sed -e 's/^\"//g' -e 's/\"$//g')\n\n" >> "games/$game_slug/$slug".md
+printf "### notes\n\n\`\`\`\n$(echo "$apiresponse" | jq '.results | .[] | .notes' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
+printf "### credits\n\n\`\`\`\n$(echo "$apiresponse" | jq '.results | .[] | .credits' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
+printf "### reason\n\n\`\`\`\n$(echo "$apiresponse" | jq '.results | .[] | .reason' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
+printf "### content\n\n\`\`\`\n$(echo "$apiresponse" | jq '.results | .[] | .content' | sed -e 's/^\"//g' -e 's/\"$//g')\n\`\`\`\n\n" >> "games/$game_slug/$slug".md
